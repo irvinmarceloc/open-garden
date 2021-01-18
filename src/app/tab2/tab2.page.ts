78bf10e6147b1a30a9d2902549c3e1d142578255
irvinmarcelo <irvinmarceloc@gmail.com>
2021-01-17 23:41:11 -0300

    Semi Completo Agregar

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { 
+  FormGroup, 
+  FormBuilder, 
+  FormControl, 
+  Validators 
+} from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  routineForm: FormGroup;
+  validation_messages = {
+    from: [{type:"required", message:"La fecha de inicio no puede quedar vacía"}],
+    to: [{type:"required", message:"La fecha de fin no puede quedar vacía"}],
+    repeat: [{type:"required", message:"Elija ¨Solo hoy¨ o ¨Cada día¨"}],
+    zone: [{type:"required", message:"Elija una zona"}]
+  }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.routineForm = this.formBuilder.group({
+      from: new  FormControl(        
+      "",
+      Validators.compose([
+        Validators.required,
+      ])),
+      to: new  FormControl(        
+      "",
+      Validators.compose([
+        Validators.required,
+      ])),
+      repeat: new  FormControl(        
+      "",
+      Validators.compose([
+        Validators.required,
+      ])),
+      zone: new  FormControl(
+      "",
+      Validators.compose([
+        Validators.required,
+      ]))
+    })
+  }
 
-  constructor() {}
 
+  Send(routineData){
+    console.log(routineData)
+  }
+
+  ngOnInit() {}
 }
