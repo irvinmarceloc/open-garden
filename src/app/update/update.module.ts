c395317862fb7c790d0ec9d39c67a7a2a60275a8
irvinmarcelo <irvinmarceloc@gmail.com>
2021-01-29 00:36:52 -0300

    V14

--- src/app/update/update.module.ts
+++ src/app/update/update.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,11 +12,13 @@ import { ComponentsModule } from '../components/components.module';
 
 import { RouterModule } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     UpdatePageRoutingModule,
     ComponentsModule,
